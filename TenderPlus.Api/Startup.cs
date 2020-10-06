336328a47078b709150c9a4986b22220473e3308
akshay chikhale <akshay.chikhale@wipro.com>
2020-10-06 09:59:14 +0530

    test

--- TenderPlus.Api/Startup.cs
+++ TenderPlus.Api/Startup.cs
@@ -1,16 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using System;
 
 namespace TenderPlus.Api
 {
@@ -27,6 +21,8 @@ namespace TenderPlus.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddCors();
 
             services.AddSwaggerGen(c =>
             {
@@ -71,6 +67,12 @@ namespace TenderPlus.Api
             });
 
             app.UseRouting();
+
+            app.UseCors(x => x
+             .AllowAnyMethod()
+             .AllowAnyHeader()
+             .SetIsOriginAllowed(origin => true) // allow any origin
+             .AllowCredentials()); // allow credentials
 
             app.UseAuthorization();
 
