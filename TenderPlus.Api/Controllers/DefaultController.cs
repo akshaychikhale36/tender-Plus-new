43c02ad5529005c4cd386a11be2df3487f9fc7e5
akshay chikhale <akshay.chikhale@wipro.com>
2020-10-06 15:02:39 +0530

    test

---
+++ TenderPlus.Api/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TenderPlus.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet]
+         public async Task<IActionResult> GetVendorInvoices()
+        {
+            return  Ok("Hi");
+        }
+    }
+}
