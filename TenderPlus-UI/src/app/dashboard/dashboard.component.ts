17662c3ab4a45601b7b02ce6a19ebb3cfbabff12
akshay chikhale <akshay.chikhale@wipro.com>
2020-12-29 10:26:26 +0530

    dashboard

--- TenderPlus-UI/src/app/dashboard/dashboard.component.ts
+++ TenderPlus-UI/src/app/dashboard/dashboard.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-
+count:number[];
   constructor() { }
 
   ngOnInit(): void {
+    this.count=[1,2,3,4,5,6,7,8,8,8,8,7]
   }
 
 }
